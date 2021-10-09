853fb1d208aa83264ce33db71ad62441b964ded7
Nick Bradley <nick@ncbradley.com>
2021-10-09 13:49:57 -0700

    Initial commit

---
+++ dot_config/private_qutebrowser/config.py
@@ -0,0 +1,3 @@
+config.load_autoconfig()
+config.bind(',p', 'spawn --userscript qute-keepassxc --key nick@ncbradley.com')
+config.source('nord-qutebrowser.py')
---
+++ dot_config/private_qutebrowser/symlink_nord-qutebrowser.py
@@ -0,0 +1,1 @@
+/home/ncbradley/aur/nord-qutebrowser/nord-qutebrowser.py
